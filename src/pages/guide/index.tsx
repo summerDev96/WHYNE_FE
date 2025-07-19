cc117e250b4a0c21843a82bbbdea7b29c296e1e4
summerdev96 <developingsummer96@gmail.com>
2025-07-19 16:38:49 +0900

    chore: 가이드 목차 수정

--- src/pages/guide/index.tsx
+++ src/pages/guide/index.tsx
@@ -4,14 +4,9 @@ const Guide = () => {
   return (
     <div className="p-10">
       <div className="text-2xl font-extrabold mb-4">컴포넌트 가이드 목록</div>
-      <div className="flex flex-col gap-2">
-        <Link href="/guide/modal/basicmodal" className="text-lg font-medium">
-          BasicModal 가이드
-        </Link>
-        <Link href="/guide/modal/confirmmodal" className="text-lg font-medium">
-          ConfirmModal 가이드
-        </Link>
-      </div>
+      <Link href="/guide/modal" className="text-lg font-medium">
+        모달 가이드
+      </Link>
     </div>
   );
 };
