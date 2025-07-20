f55d80bf8992f6a8b86a2eebb9d9e2e0e26f227a
summerdev96 <developingsummer96@gmail.com>
2025-07-20 19:45:39 +0900

    fix: 필터 범위 변경 및 전체 값 all로 변경

--- src/components/common/Filter/Filter.tsx
+++ src/components/common/Filter/Filter.tsx
@@ -14,6 +14,7 @@ const Filter = () => {
     rating,
     setRating,
   } = useFilterStore();
+
   const wineTypeOptions: WineType[] = ["Red", "White", "Sparkling"];
   const priceRange: [number, number] = [minPrice, maxPrice];
 
@@ -55,21 +56,25 @@ const Filter = () => {
         {/* todo: 공통 라디오, 라벨 컴포넌트로 변경 필요 */}
         <RadioGroup value={rating} onValueChange={setRating}>
           <div className="flex items-center gap-3">
-            {/* 전체인 경우 보내야 하는 값을 몰라서, 임의로 value 0으로 설정하였습니다. */}
-            <RadioGroupItem value="0" id="r1" />
-            <Label htmlFor="r1">전체</Label>
+            {/* 전체인 경우 rating값을 아예 안보내는 것 같아 임의로 all로 설정 */}
+            <RadioGroupItem value="all" id="rate-all" />
+            <Label htmlFor="rate-all">전체</Label>
           </div>
           <div className="flex items-center gap-3">
-            <RadioGroupItem value="4.9" id="r2" />
-            <Label htmlFor="r2">4.8 - 5.0</Label>
+            <RadioGroupItem value="4.6" id="rate1" />
+            <Label htmlFor="rate1">4.5 - 5.0</Label>
           </div>
           <div className="flex items-center gap-3">
-            <RadioGroupItem value="4.1" id="r3" />
-            <Label htmlFor="r3">4.0 - 4.5</Label>
+            <RadioGroupItem value="4.1" id="rate2" />
+            <Label htmlFor="rate2">4.0 - 4.5</Label>
           </div>
           <div className="flex items-center gap-3">
-            <RadioGroupItem value="3.1" id="r4" />
-            <Label htmlFor="r4">3.0 - 4.0</Label>
+            <RadioGroupItem value="3.6" id="rate3" />
+            <Label htmlFor="rate3">3.5 - 4.0</Label>
+          </div>
+          <div className="flex items-center gap-3">
+            <RadioGroupItem value="3.1" id="rate4" />
+            <Label htmlFor="rate4">3.0 - 3.5</Label>
           </div>
         </RadioGroup>
       </div>
