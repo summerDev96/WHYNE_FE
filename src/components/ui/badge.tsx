f144cd6bd2621592ddb05488a8c0254cfb01dc94
llmojoll <wnsgusqkqh21@gmail.com>
2025-07-20 16:44:41 +0900

    Feat : badge컴포넌트 구현

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -12,14 +12,14 @@ const badgeVariants = cva(
       variant: {
         //모달에서 테블릿,데탑 h-46 px-2.5 py-4.5 별로 조금씩다름 나중에 조정필요
         chooseFlavor:
-          "bg-white text-black border border-gray-300 hover:bg-purpleDark hover:text-white hover:border-purpleDark h-[36px] rounded-full px-2.5 py-1.5",
+          "bg-white text-black border border-gray-300 hover:bg-primary hover:text-white hover:border-primary h-[36px] rounded-full px-2.5 py-1.5",
         chooseWineType:
-          "bg-white text-black border border-gray-300 hover:bg-purpleDark hover:text-white hover:border-purpleDark h-[36px] md:h-[42px] rounded-full px-2.5 py-1.5",
+          "bg-white text-black border border-gray-300 hover:bg-primary hover:text-white hover:border-primary h-[36px] md:h-[42px] rounded-full px-2.5 py-1.5",
         priceBadge:
-          "bg-purpleLight text-purpleDark h-[29px] md:h-[42px] px-2.5 py-[2.5px] md:px-2.5 md:py-1.5 rounded-[10px] md:rounded-[12px]",
+          "bg-primary-100 text-primary h-[29px] md:h-[42px] px-2.5 py-[2.5px] md:px-2.5 md:py-1.5 rounded-[10px] md:rounded-[12px]",
         flavor:
           "bg-white text-black border border-gray-300 h-[36px] rounded-full px-2.5 py-1.5",
-        star: "bg-purpleLight text-purpleDark h-[36px] md:h-[42px] px-2.5 py-1.5 md:px-[15px] md:py-2 rounded-[12px]",
+        star: "bg-primary-100 text-primary h-[36px] md:h-[42px] px-2.5 py-1.5 md:px-[15px] md:py-2 rounded-[12px]",
         taste:
           "bg-gray-100 text-gray-500 h-[30px] md:h-[28px] px-[11px] py-[5px] md:px-[15px] md:py-[5px] rounded-[6px]",
       },
