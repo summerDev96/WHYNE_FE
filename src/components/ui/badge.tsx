da1719a653cb31aa276508931114df5ed2414151
llmojoll <wnsgusqkqh21@gmail.com>
2025-07-20 13:50:56 +0900

    Feat : badgeGuide, badge컴포 variants 구현

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -5,8 +5,6 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 // 색상, 폰트 조정필요 (배경, 폰트크기,굴기, 테두리(borer))
-//24 25 26   ,28 29,32 34 ,38 ,42,46 다양한 높이, raius(6,8 ,100), 값을 그냥 받아서 쓰는게 좋고 편하지 않을까?
-// 46 36
 const badgeVariants = cva(
   "inline-flex items-center justify-center  rounded-md text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
@@ -18,16 +16,12 @@ const badgeVariants = cva(
         chooseWineType:
           "bg-white text-black border border-gray-300 hover:bg-purpleDark hover:text-white hover:border-purpleDark h-[36px] md:h-[42px] rounded-full px-2.5 py-1.5",
         priceBadge:
-          "bg-purpleLight text-purpleDark h-[29px] md:h-[42px] px-2.5 py-[2.5px] md:px-2.5 md:py-1.5",
+          "bg-purpleLight text-purpleDark h-[29px] md:h-[42px] px-2.5 py-[2.5px] md:px-2.5 md:py-1.5 rounded-[10px] md:rounded-[12px]",
         flavor:
           "bg-white text-black border border-gray-300 h-[36px] rounded-full px-2.5 py-1.5",
-        star: "bg-purpleLight text-purpleDark h-[36px] md:h-[42px] px-2.5 py-1.5 md:px-[15px] md:py-2",
-        taste: "bg-gray-100 text-gray-500 ",
-        purpleDark:
-          "bg-purpleDark text-white hover:bg-purpleLight hover:text-purpleDark",
-        white: "bg-white text-black border border-gray-300",
-        purpleLight: "bg-purpleLight text-purpleDark",
-        gray: "bg-white text-gray-500 border border-gray-300",
+        star: "bg-purpleLight text-purpleDark h-[36px] md:h-[42px] px-2.5 py-1.5 md:px-[15px] md:py-2 rounded-[12px]",
+        taste:
+          "bg-gray-100 text-gray-500 h-[30px] md:h-[28px] px-[11px] py-[5px] md:px-[15px] md:py-[5px] rounded-[6px]",
       },
     },
     defaultVariants: {
--- src/pages/guide/badgeGuide/badges.tsx
+++ src/pages/guide/badgeGuide/badges.tsx
@@ -2,22 +2,43 @@ import { Badge } from "@/components/ui/badge";
 
 export default function badges() {
   return (
-    <div>
-      <input
-        type="radio"
-        id="mail"
-        name="contact"
-        value="mail"
-        className="peer hidden" // 숨기되 상태 감지는 유지
-      />
-      <label htmlFor="mail">
-        <Badge>우편</Badge>
-      </label>
-      <button type="submit">제출</button>
-      <Badge variant="chooseWineType">레드와인</Badge>
-      <Badge variant="priceBadge">2000원</Badge>
-      <Badge variant="flavor">향12</Badge>
-      <Badge variant="star">☆ ★</Badge>
+    <div style={{ marginBottom: "30px" }}>
+      variant : chooseFlavor(기본값)
+      <div style={{ marginBottom: "30px", display: "flex", gap: "10px" }}>
+        <Badge>체리</Badge>
+        <Badge>베리</Badge>
+        <Badge>바닐라</Badge>
+        <Badge>후추</Badge>
+        <Badge>흙</Badge>
+      </div>
+      variant : chooseWineType
+      <div style={{ marginBottom: "30px", display: "flex", gap: "10px" }}>
+        <Badge variant="chooseWineType">Red</Badge>
+        <Badge variant="chooseWineType">White</Badge>
+        <Badge variant="chooseWineType">Sparking</Badge>
+      </div>
+      variant : priceBadge
+      <div style={{ marginBottom: "30px", display: "flex", gap: "10px" }}>
+        <Badge variant="priceBadge">$20000</Badge>
+      </div>
+      variant : flavor
+      <div style={{ marginBottom: "30px", display: "flex", gap: "10px" }}>
+        <Badge variant="flavor">체리</Badge>
+        <Badge variant="flavor">오크</Badge>
+        <Badge variant="flavor">꽃</Badge>
+        <Badge variant="flavor">카라멜</Badge>
+      </div>
+      variant : star
+      <div style={{ marginBottom: "30px", display: "flex", gap: "10px" }}>
+        <Badge variant="star">★5.0</Badge>
+      </div>
+      variant : taste
+      <div style={{ marginBottom: "30px", display: "flex", gap: "10px" }}>
+        <Badge variant="taste">바디감</Badge>
+        <Badge variant="taste">타닌</Badge>
+        <Badge variant="taste">당도</Badge>
+        <Badge variant="taste">산미</Badge>
+      </div>
     </div>
   );
 }
