2888e2f364e5bcecee32f3e33c15c5e7007ac7d5
이관현\Luganic <rhksgus509@gmail.com>
2025-07-20 11:48:08 +0900

    package.json, tailwind.config.ts 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,19 @@
-import shadTheme from './src/styles/shadTheme';
-import type { Config } from 'tailwindcss';
-import animate from 'tailwindcss-animate';
+import shadTheme from "./src/styles/shadTheme";
+import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 const config: Config = {
-  darkMode: ['class'],
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+      },
       ...shadTheme,
     },
   },
