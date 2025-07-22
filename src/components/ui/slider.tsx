8e4a500f4d98d1d192cc5a5eef945dcfa0ac8d51
youjin-hub <leeyoujin0704@naver.com>
2025-07-22 17:36:56 +0900

    feat: 기존 shadcn 슬라이더 수정

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -3,6 +3,18 @@ import * as React from 'react';
 import * as SliderPrimitive from '@radix-ui/react-slider';
 
 import { cn } from '@/lib/utils';
+const trackClass = cn(
+  'relative h-1.5 w-full grow overflow-hidden',
+  'rounded-full bg-gray-100 border border-gray-300 ',
+);
+
+const rangeClass = cn('absolute h-full', 'bg-transparent');
+
+const thumbClass = cn(
+  'block h-4 w-4 rounded-full border border-primary/50 bg-primary shadow',
+  'transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring',
+  'disabled:pointer-events-none disabled:opacity-50',
+);
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
@@ -13,12 +25,13 @@ const Slider = React.forwardRef<
     className={cn('relative flex w-full touch-none select-none items-center', className)}
     {...props}
   >
-    <SliderPrimitive.Track className='relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20'>
-      <SliderPrimitive.Range className='absolute h-full bg-primary' />
+    <SliderPrimitive.Track className={trackClass}>
+      <SliderPrimitive.Range className={rangeClass} />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className='block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50' />
+    <SliderPrimitive.Thumb className={thumbClass} />
   </SliderPrimitive.Root>
 ));
+
 Slider.displayName = SliderPrimitive.Root.displayName;
 
 export { Slider };
