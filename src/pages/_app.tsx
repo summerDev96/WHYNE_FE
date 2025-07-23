92567e3d07cac38ede5a6650da8123388419f214
summerdev96 <developingsummer96@gmail.com>
2025-07-23 16:20:27 +0900

    feat: app 파일 내 react-query 프로바이더 및 클라이언트 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import clsx from 'clsx';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -8,21 +9,15 @@ import Gnb from '@/components/common/Gnb';
 
 import type { AppProps } from 'next/app';
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
-  const pagesWithoutGnb = ['/login', '/signup', '/_error'];
+  const pagesWithoutGnb = ['/login', '/signup', '/signin', '/_error'];
   const hideHeader = pagesWithoutGnb.includes(pathname);
 
   return (
     <>
-      {!hideHeader && <Gnb />}
-      <div
-        className={clsx({
-          'pt-[70px] md:pt-[100px] xl:pt-[110px]': !hideHeader,
-        })}
-      >
-        <Component {...pageProps} />
-      </div>
       <Head>
         <title>WINE</title>
         <meta charSet='UTF-8' />
@@ -35,6 +30,16 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property='og:url' content='' />
         <meta property='og:type' content='website' />
       </Head>
+      <QueryClientProvider client={queryClient}>
+        {!hideHeader && <Gnb />}
+        <div
+          className={clsx({
+            'pt-[70px] md:pt-[100px] xl:pt-[110px]': !hideHeader,
+          })}
+        >
+          <Component {...pageProps} />
+        </div>
+      </QueryClientProvider>
     </>
   );
 }
