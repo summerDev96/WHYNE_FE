5d074cb5d3fe2ff4857394a60215af214c284cbb
summerdev96 <developingsummer96@gmail.com>
2025-07-23 16:13:55 +0900

    feat: 공통 head 태그 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 
 import clsx from 'clsx';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import Gnb from '@/components/common/Gnb';
@@ -22,6 +23,18 @@ export default function App({ Component, pageProps }: AppProps) {
       >
         <Component {...pageProps} />
       </div>
+      <Head>
+        <title>WINE</title>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='description' content='와인 리뷰 사이트' />
+        <meta property='og:title' content='WINE' />
+        <meta property='og:description' content='와인 리뷰 사이트' />
+        {/* todo: 배포 후 이미지, url 변경 필요 */}
+        <meta property='og:image' content='' />
+        <meta property='og:url' content='' />
+        <meta property='og:type' content='website' />
+      </Head>
     </>
   );
 }
