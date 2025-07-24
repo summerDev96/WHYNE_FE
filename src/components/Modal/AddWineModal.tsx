b84ac4b27abc58d59fc57d747ec75c93b217df6f
llmojoll <wnsgusqkqh21@gmail.com>
2025-07-24 18:34:16 +0900

    Fix : QA 피드백반영 버튼 색상수정, X버튼 삭제, 플레이스홀더 추가

--- src/components/Modal/AddWineModal.tsx
+++ src/components/Modal/AddWineModal.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 
 import axiosInstance from '@/api/axios';
 import CameraIcon from '@/assets/camera.svg';
+import DropdownIcon from '@/assets/dropdowntriangle.svg';
 
 import SelectDropdown from '../common/dropdown/SelectDropdown';
 import Input from '../common/Input';
@@ -15,6 +16,7 @@ interface WineForm {
   winePrice: number;
   wineOrigin: string;
   wineImage: FileList;
+  wineType: string;
 }
 
 ////api 테스트용////
@@ -50,7 +52,7 @@ export const PostWine = async (data: {
 
 const AddWineModal = () => {
   const [showRegisterModal, setShowRegisterModal] = useState(false);
-  const [category, setCategory] = useState('Red');
+  const [category, setCategory] = useState('');
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const [previewImage, setPreviewImage] = useState<string | null>(null);
 
@@ -73,8 +75,12 @@ const AddWineModal = () => {
     handleSubmit,
     formState: { errors },
     clearErrors,
+    trigger,
+    setValue,
     reset,
-  } = useForm<WineForm>();
+  } = useForm<WineForm>({
+    mode: 'onBlur',
+  });
 
   // const onSubmit = (data: WineForm) => {
   //   const fullData = {
@@ -99,14 +105,22 @@ const AddWineModal = () => {
         region: form.wineOrigin,
         image: imageUrl,
         price: Number(form.winePrice),
-        type: category.toUpperCase() as 'RED' | 'WHITE' | 'SPARKLING',
-        category,
+        type: form.wineType.toUpperCase() as 'RED' | 'WHITE' | 'SPARKLING',
       };
 
       await PostWine(requestData);
 
       console.log('와인등록완료');
-      reset();
+      reset({
+        wineName: '',
+        winePrice: NaN,
+        wineOrigin: '',
+        wineImage: {} as FileList,
+        wineType: '',
+      });
+      setPreviewImage(null);
+      fileInputRef.current && (fileInputRef.current.value = '');
+      setCategory('');
       setShowRegisterModal(false);
     } catch (error) {
       console.error('와인등록실패', error);
@@ -120,15 +134,24 @@ const AddWineModal = () => {
     { label: 'Sparkling', value: 'Sparkling' },
   ];
 
-  const selectedCategoryLabel =
-    categoryOptions.find((opt) => opt.value === category)?.label ?? '선택';
+  const selectedCategoryLabel = categoryOptions.find((opt) => opt.value === category)?.label;
 
   //모달창 끄면 리셋되게
   const closeModalReset = (isOpen: boolean) => {
     setShowRegisterModal(isOpen);
     if (!isOpen) {
-      reset();
-      setPreviewImage(null); //이미지 미리보기 초기화
+      setTimeout(() => {
+        reset({
+          wineName: '',
+          winePrice: NaN,
+          wineOrigin: '',
+          wineImage: {} as FileList,
+          wineType: '',
+        });
+        setCategory('');
+        setPreviewImage(null); //이미지 미리보기 초기화
+        fileInputRef.current && (fileInputRef.current.value = '');
+      }, 50);
     }
   };
   ////
@@ -142,14 +165,24 @@ const AddWineModal = () => {
         title='와인 등록'
         open={showRegisterModal}
         onOpenChange={closeModalReset}
+        showCloseButton={false}
         buttons={
           <div className='flex gap-2'>
             <Button
               onClick={() => {
-                reset();
+                reset({
+                  wineName: '',
+                  winePrice: NaN,
+                  wineOrigin: '',
+                  wineImage: {} as FileList,
+                  wineType: '',
+                });
+                setCategory('');
+                setPreviewImage(null);
+                fileInputRef.current && (fileInputRef.current.value = '');
                 setShowRegisterModal(false);
               }}
-              variant='onlyCancel'
+              variant='purpleLight'
               size='xl'
               className='w-[96px] md:w-[108px]'
               fontSize='lg'
@@ -221,12 +254,32 @@ const AddWineModal = () => {
           <SelectDropdown
             value={category}
             options={categoryOptions}
-            onChange={setCategory}
+            onChange={(value) => {
+              setCategory(value);
+              setValue('wineType', value, { shouldValidate: true });
+              trigger('wineType');
+            }}
+            placeholder='타입 선택'
             trigger={
-              <button className='w-full px-4 py-2 border rounded text-left'>
-                {selectedCategoryLabel}
+              <button
+                className={`w-full h-[42px] md:h-[48px] px-4 py-2 border rounded-[12px] md:rounded-[16px] text-left ${category ? 'text-black' : 'text-gray-500'}`}
+              >
+                <>
+                  <span>{selectedCategoryLabel || '타입 선택'}</span>
+                  <DropdownIcon className='ml-2 w-4 h-4 bg-black' />
+                </>
               </button>
             }
+          />
+          {errors.wineType?.message && (
+            <p className='text-red-500 mt-1'>{errors.wineType.message}</p>
+          )}
+          <Input
+            id='wineType'
+            type='hidden'
+            {...register('wineType', {
+              required: '타입을 선택해 주세요.',
+            })}
           />
           <p className='custom-text-md-medium md:custom-text-lg-medium mb-[10px] md:mb-[12px] mt-[22px] md:mt-[24px]'>
             와인 사진
