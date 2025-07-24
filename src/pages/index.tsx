b60bb5ff425ebf62898ab533fe4de74f57ff5d84
llmojoll <wnsgusqkqh21@gmail.com>
2025-07-25 02:17:34 +0900

    Feat : 렌딩페이지 와인카드들 추가 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 
+import { ImageCard } from '@/components/common/card/ImageCard';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
@@ -21,18 +22,83 @@ export default function Home() {
               한 곳에서 관리하는 <br />
               나만의 와인창고
             </span>
-            {/* 카드에 와인넣기 */}
-            <div className='relative w-[720px] h-[420px] mt-auto mx-auto translate-y-[80px] md:translate-y-[40px] lg:translate-y-[120px]'>
-              {/* 흰색 박스 translate-y 아래로-ㅁㅁ만큼 밀어내기 */}
-              <div className='absolute top-4 left-1/2 w-[240px] h-[185px] bg-white rounded-lg z-30 -translate-x-1/2'></div>
+            <div className='relative w-[720px] h-[420px] mt-auto mx-auto translate-y-[80px] md:translate-y-[40px] lg:translate-y-[120px] scale-[1] md:scale-[0.7] lg:scale-[1]'>
+              {/* 기준 박스 translate-y 아래로-ㅁㅁ만큼 밀어내기 */}
+              <div className='absolute top-4 left-1/2 w-[240px] h-[188px] bg-white rounded-xl z-30 -translate-x-1/2 shadow-xl'>
+                <ImageCard
+                  imageSrc='/assets/lendingwine3.png'
+                  imageClassName='h-full w-[44px] object-contain '
+                >
+                  <div className='flex flex-col ml-[8px] w-[100px]'>
+                    <span className='font-extrabold text-4xl text-gray-800'>4.3</span>
+                    {/* 모달쪽에 구현해둔 별점이 있음 */}
+                    <span>별점 나중에</span>
+                    <p className='custom-text-xs-regular text-gray-500'>
+                      Sentinel Carbernet Sauvignon 2016
+                    </p>
+                  </div>
+                </ImageCard>
+              </div>
 
               {/* 좌우 위 박스 */}
-              <div className='absolute top-10 left-[calc(50%-300px)] w-[240px] h-[185px] bg-[#D842DB] rounded-lg z-20'></div>
-              <div className='absolute top-10 left-[calc(50%+60px)] w-[240px] h-[185px] bg-black rounded-lg z-20'></div>
+              <div className='absolute top-10 left-[calc(50%-340px)] w-[240px] h-[185px] rounded-xl z-20 shadow-xl'>
+                <ImageCard
+                  imageSrc='/assets/lendingwine1.png'
+                  imageClassName='h-full w-[44px] object-contain '
+                >
+                  <div className='flex flex-col ml-[8px] w-[100px]'>
+                    <span className='font-extrabold text-4xl text-gray-800'>4.3</span>
+                    <span>별점 나중에</span>
+                    <p className='custom-text-xs-regular text-gray-500'>
+                      Sentinel Carbernet Sauvignon 2016
+                    </p>
+                  </div>
+                </ImageCard>
+              </div>
+              <div className='absolute top-10 left-[calc(50%+100px)] w-[240px] h-[185px] rounded-xl z-20 shadow-xl'>
+                <ImageCard
+                  imageSrc='/assets/lendingwine1.png'
+                  imageClassName='h-full w-[44px] object-contain '
+                >
+                  <div className='flex flex-col ml-[8px] w-[100px]'>
+                    <span className='font-extrabold text-4xl text-gray-800'>4.3</span>
+                    <span>별점 나중에</span>
+                    <p className='custom-text-xs-regular text-gray-500'>
+                      Sentinel Carbernet Sauvignon 2016
+                    </p>
+                  </div>
+                </ImageCard>
+              </div>
 
               {/* 좌우 아래 박스 */}
-              <div className='absolute top-[80px] left-[calc(50%-400px)] w-[240px] h-[185px] bg-primary rounded-lg z-10'></div>
-              <div className='absolute top-[80px] left-[calc(50%+160px)] w-[240px] h-[185px] bg-[#6A42DB] rounded-lg z-10'></div>
+              <div className='absolute top-[80px] left-[calc(50%-480px)] w-[240px] h-[185px] bg-primary rounded-xl z-10 shadow-xl'>
+                <ImageCard
+                  imageSrc='/assets/lendingwine1.png'
+                  imageClassName='h-full w-[44px] object-contain '
+                >
+                  <div className='flex flex-col ml-[8px] w-[100px]'>
+                    <span className='font-extrabold text-4xl text-gray-800'>4.3</span>
+                    <span>별점 나중에</span>
+                    <p className='custom-text-xs-regular text-gray-500'>
+                      Sentinel Carbernet Sauvignon 2016
+                    </p>
+                  </div>
+                </ImageCard>
+              </div>
+              <div className='absolute top-[80px] left-[calc(50%+240px)] w-[240px] h-[185px] bg-[#6A42DB] rounded-xl z-10 shadow-xl'>
+                <ImageCard
+                  imageSrc='/assets/lendingwine1.png'
+                  imageClassName='h-full w-[44px] object-contain '
+                >
+                  <div className='flex flex-col ml-[8px] w-[100px]'>
+                    <span className='font-extrabold text-4xl text-gray-800'>4.3</span>
+                    <span>별점 나중에</span>
+                    <p className='custom-text-xs-regular text-gray-500'>
+                      Sentinel Carbernet Sauvignon 2016
+                    </p>
+                  </div>
+                </ImageCard>
+              </div>
             </div>
           </div>
         </div>
@@ -40,8 +106,8 @@ export default function Home() {
 
       <main>
         <div className='mx-auto md:px-[32px] xl:px-0 max-w-[640px]  min-w-[343px] mt-12 md:mt-[80px] lg:mt-[160px]'>
-          <div className='relative bg-[#EBEEF4] h-[424px] md:h-[320px] mb-[48px] md:mb-[96px]'>
-            <div className='relative top-[24px] left-[24px]'>
+          <div className='relative bg-[#EBEEF4] h-[424px] md:h-[320px] mb-[48px] md:mb-[96px] rounded-2xl'>
+            <div className='absolute top-[24px] md:top-[56px] left-[24px] md:left-[32px]'>
               <span className='custom-text-2lg-bold md:custom-text-xl-bold'>
                 매달 새롭게 만나는 <br />
                 와인 추천 콘탠츠
@@ -50,10 +116,42 @@ export default function Home() {
                 매달 다양한 인기 와인을 만나보세요
               </p>
             </div>
-            <div className='absolute w-[290px] md:w-[356px] h-[241px] md:h-[277px] right-0 bottom-[24px] md:bottom-0 bg-gray-100'></div>
+            <div className='absolute w-[290px] md:w-[356px] h-[241px] md:h-[277px] right-0 bottom-[24px] md:bottom-0 bg-gray-100 overflow-hidden'>
+              <span className='absolute text-[#7E7E7E] md:text-[#50545B] custom-text-lg-bold md:custom-text-2lg-bold left-5 top-5'>
+                이번 달 추천 와인
+              </span>
+              <div className='flex gap-2.5 ml-5 mt-[61px] '>
+                <div className='w-[193px] flex-shrink-0'>
+                  <ImageCard
+                    className='bg-white h-[160px] rounded-lg border-none overflow-hidden'
+                    imageSrc='/assets/lendingwine3.png'
+                    imageClassName='h-[165px] w-[44px] object-contain '
+                  >
+                    <div className='flex flex-col ml-[9px] w-[80px]'>
+                      <span className='font-extrabold text-4xl text-gray-800'>4.3</span>
+                      <span>별점 나중에</span>
+                      <p className='text-[10px] text-gray-500'>Sentinel Carbernet Sauvignon 2016</p>
+                    </div>
+                  </ImageCard>
+                </div>
+                <div className='w-[193px] flex-shrink-0'>
+                  <ImageCard
+                    className='bg-white h-[160px] rounded-lg border-none overflow-hidden'
+                    imageSrc='/assets/lendingwine3.png'
+                    imageClassName='h-[165px] w-[44px] object-contain '
+                  >
+                    <div className='flex flex-col ml-[9px] w-[80px]'>
+                      <span className='font-extrabold text-4xl text-gray-800'>4.3</span>
+                      <span>별점 나중에</span>
+                      <p className='text-[10px] text-gray-500'>Sentinel Carbernet Sauvignon 2016</p>
+                    </div>
+                  </ImageCard>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className='relative bg-[#EBEEF4] h-[424px] md:h-[320px] mb-[48px] md:mb-[96px]'>
-            <div className='relative top-[24px] left-[24px]'>
+          <div className='relative bg-[#EBEEF4] h-[424px] md:h-[320px] mb-[48px] md:mb-[96px] rounded-2xl overflow-hidden'>
+            <div className='absolute top-[24px] md:top-[55px] left-[24px] md:left-[32px]'>
               <span className='custom-text-2lg-bold md:custom-text-xl-bold'>
                 다양한 필터로 찾는
                 <br />내 맞춤 와인
@@ -63,9 +161,23 @@ export default function Home() {
                 나에게 맞는 와인을 쉽게 검색해요.
               </p>
             </div>
+            <Image
+              width={280}
+              height={100}
+              alt='필터로 찾은 내 맞춤 와인'
+              src='/assets/lendingwinecard.png'
+              className='absolute right-[-10px] top-[110px] md:top-[65px]'
+            />
+            <div
+              className='pointer-events-none absolute inset-0 rounded-xl'
+              style={{
+                background:
+                  'linear-gradient(to top, #EBEEF4 0%, transparent 5%, transparent 95%, #EBEEF4 100%)',
+              }}
+            />
           </div>
-          <div className='bg-[#EBEEF4] h-[424px] md:h-[320px] mb-[64px] md:mb-[80px] lg:mb-[104px]'>
-            <div className='relative top-[24px] left-[24px]'>
+          <div className='relative bg-[#EBEEF4] h-[424px] md:h-[320px] mb-[64px] md:mb-[80px] lg:mb-[104px] rounded-2xl overflow-hidden  '>
+            <div className='absolute top-[24px] md:top-[55px] left-[24px] md:left-[28px]'>
               <span className='custom-text-2lg-bold md:custom-text-xl-bold'>
                 직관적인
                 <br />
@@ -77,6 +189,20 @@ export default function Home() {
                 쉽고 빠르게 와인 리뷰를 살펴보세요/
               </p>
             </div>
+            <Image
+              width={272}
+              height={100}
+              alt='와인 리뷰 예시 이미지'
+              src='/assets/lendingreviewcard.png'
+              className='absolute right-[-10px] top-[120px] md:right-[42px] md:top-[-65px]'
+            />
+            <div
+              className='pointer-events-none absolute inset-0 rounded-xl'
+              style={{
+                background:
+                  'linear-gradient(to top, #EBEEF4 0%, transparent 5%, transparent 95%, #EBEEF4 100%)',
+              }}
+            />
           </div>
         </div>
       </main>
