3bc60ff39b6d81c38ac8d9f450b233477bbaf7ce
youjin-hub <leeyoujin0704@naver.com>
2025-07-25 15:32:50 +0900

    feat: 무한스크롤 구현을 위한 목업 데이터 page 분리, 리스트 컴포넌트에서 데이터 가져오도록 수정 (기존은 prop으로 내려주는 형식)

--- src/pages/myprofile/index.tsx
+++ src/pages/myprofile/index.tsx
@@ -1,34 +1,32 @@
-// pages/myprofile.tsx
 import React, { useState } from 'react';
 
-import { mockUserReviewsResponse, mockUserWinesResponse } from './mockUser';
+import { mockMyReviewsPage1, mockMyWinesPage1 } from './mockUser';
 import Profile from './Profile';
 import { ReviewList } from './ReviewList';
 import { TabNav } from './Tab';
 import { WineList } from './WineList';
 
 export default function MyProfile() {
+  // 탭 상태: 'reviews' | 'wines'
   const [tab, setTab] = useState<'reviews' | 'wines'>('reviews');
 
   return (
     <div className='min-h-screen'>
-      <main className='max-w-6xl mx-auto p-4 gap-6 flex flex-col xl:flex-row  '>
-        {/* Profile */}
+      <main className='max-w-6xl mx-auto p-4 gap-6 flex flex-col xl:flex-row'>
+        {/* 프로필 섹션 */}
         <Profile nickname='홍길동' profileImageUrl='https://picsum.photos/64' />
 
-        {/* Tabs */}
+        {/* 탭 & 리스트 섹션 */}
         <div className='flex flex-col flex-1'>
           <TabNav
             current={tab}
             onChange={setTab}
-            reviewsCount={mockUserReviewsResponse.totalCount}
-            winesCount={mockUserWinesResponse.totalCount}
+            reviewsCount={mockMyReviewsPage1.totalCount}
+            winesCount={mockMyWinesPage1.totalCount}
           />
-          {tab === 'reviews' ? (
-            <ReviewList items={mockUserReviewsResponse.list} />
-          ) : (
-            <WineList items={mockUserWinesResponse.list} />
-          )}
+
+          {/* 탭에 따라 ReviewList 또는 WineList에 props 전달 */}
+          {tab === 'reviews' ? <ReviewList /> : <WineList />}
         </div>
       </main>
     </div>
