67bbeff11c303cd54dd78fbfa1516f2c91be58d8
llmojoll <wnsgusqkqh21@gmail.com>
2025-07-25 17:37:45 +0900

    Style : 반응형 수정, 배경색 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,23 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div className='mx-auto px-[16px] md:px-[20px] xl:px-0 max-w-[1140px]  min-w-[343px] mt-6 xl:mt-[80px]'>
-      <HeroSection />
-      <main>
-        <ContentSection />
-      </main>
-      <footer>
-        <Button
-          variant='purpleDark'
-          size='onlyLanding'
-          width='onlyLanding'
-          className='block mx-auto mb-[62px]'
-        >
-          와인 보러가기
-        </Button>
-      </footer>
+    <div className='bg-gray-100'>
+      <div className='mx-auto px-[16px] md:px-[20px] xl:px-0 max-w-[1140px]  min-w-[343px] mt-6 xl:mt-[80px]'>
+        <HeroSection />
+        <main>
+          <ContentSection />
+        </main>
+        <footer>
+          <Button
+            variant='purpleDark'
+            size='onlyLanding'
+            width='onlyLanding'
+            className='block mx-auto mb-[62px]'
+          >
+            와인 보러가기
+          </Button>
+        </footer>
+      </div>
     </div>
   );
 }
