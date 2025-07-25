aff7213823c8c1ee5ab55cdfc3941b55c7f1aa30
summerdev96 <developingsummer96@gmail.com>
2025-07-25 13:50:13 +0900

    feat: FormInput 컴포넌트 비밀번호 아이콘 추가

--- src/components/common/FormInput.tsx
+++ src/components/common/FormInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   useFormContext,
@@ -8,6 +8,8 @@ import {
   type Path,
 } from 'react-hook-form';
 
+import PasswordIconOff from '@/assets/icons/visibility_off.svg';
+import PasswordIconOn from '@/assets/icons/visibility_on.svg';
 import Input from '@/components/common/Input';
 
 interface FormInputProps<T> {
@@ -24,6 +26,9 @@ const FormInput = <T extends FieldValues>(props: FormInputProps<T>) => {
   const methods = useFormContext();
   const { errors } = useFormState({ name });
 
+  const [passwordType, setPasswordType] = useState(type);
+  const isPassword = type === 'password';
+
   function getErrorMessage(errors: FieldErrors) {
     const errMsg = errors[name]?.message;
     const result = errMsg && typeof errMsg === 'string' ? errMsg : '';
@@ -32,19 +37,34 @@ const FormInput = <T extends FieldValues>(props: FormInputProps<T>) => {
 
   const { onChange: formOnChange, ...inputProps } = methods.register(name);
 
+  const togglePasswordIcon = () => {
+    setPasswordType((prev) => (prev === 'password' ? 'text' : 'password'));
+    console.log('아이콘 클릭 시 토글');
+  };
+
   return (
-    <Input
-      {...inputProps}
-      type={type}
-      id={id}
-      name={name}
-      placeholder={placeholder}
-      errorMessage={getErrorMessage(errors)}
-      onChange={(e) => {
-        formOnChange(e);
-        onChange?.(e);
-      }}
-    />
+    <div className='relative'>
+      <Input
+        {...inputProps}
+        type={isPassword ? passwordType : type}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        errorMessage={getErrorMessage(errors)}
+        onChange={(e) => {
+          formOnChange(e);
+          onChange?.(e);
+        }}
+      />
+      {isPassword && (
+        <span
+          className='absolute right-4 top-1/2 -translate-y-1/2 w-6 h-6 text-gray-500'
+          onClick={togglePasswordIcon}
+        >
+          {passwordType === 'password' ? <PasswordIconOff /> : <PasswordIconOn />}
+        </span>
+      )}
+    </div>
   );
 };
 
