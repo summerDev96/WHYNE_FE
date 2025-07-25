8f8152ac6db950f5fe8a26927cb6a3c9ed2689d1
김성주 <aaa8757k@gmail.com>
2025-07-25 12:40:40 +0900

    chore:스테이징 누락된 파일 추가

--- src/stores/reviewCardStore.ts
+++ src/stores/reviewCardStore.ts
@@ -60,8 +60,9 @@ export const reviewStore = create<ReviewStates>((set) => ({
 
 //use-sync-external-store 패키지 설치 후 useStoreWithEqualityFn으로 타입 지정해야 shallow제대로 인식
 //-> 이거 외에 패키지 설치 안하는 방식으로 이것저것 해봤는데 도무지 못 찾겠네요 ㅠ
+//-> 일단 쓰긴했는데 왜 이 패키지가 필요한지 잘 모르겠습니다. 예전에 그냥 리액트+js로만 했을 때는 없어도 잘 인식했던 것 같은데...
 
-//aroma가 배열이라 shallow필요할 것 같긴 함
+//원시타입 === 비교/ 객체,배열 얕은 비교
 const useReviewCardStore = <T>(selector: (state: ReviewStates) => T): T =>
   useStoreWithEqualityFn(reviewStore, selector, shallow);
 
