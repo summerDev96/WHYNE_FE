dda42c4b16ddeb5b242a3d97e6ce502a30810aa2
llmojoll <wnsgusqkqh21@gmail.com>
2025-07-25 21:49:09 +0900

    Feat : 필터 모달 UI 중간점검

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+import FilterModal from '@/components/Modal/FilterModal/FilterModal';
+
 export default function Home() {
-  return <main>메인</main>;
+  return (
+    <main>
+      메인
+      <FilterModal />
+    </main>
+  );
 }
