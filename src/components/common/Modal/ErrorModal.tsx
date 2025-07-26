11cfdb4d2ab17dff15fc15c421cbc799b21e24c3
summerdev96 <developingsummer96@gmail.com>
2025-07-26 22:04:38 +0900

    fix: 에러 모달 props 수정

--- src/components/common/Modal/ErrorModal.tsx
+++ src/components/common/Modal/ErrorModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
 import Modal from './Modal';
 
@@ -9,27 +10,31 @@ interface ErrorModalProps {
   onOpenChange: (open: boolean) => void;
   onConfirm?: () => void;
   showCloseButton?: boolean;
-  errorMessage: string;
   children?: React.ReactNode;
+  className?: string;
 }
+
+/* 기존에 errorMessage를 props로 받았는데,
+줄바꿈 처리가 불편할 것 같아 children으로 받도록 수정하였습니다 */
 
 const ErrorModal = ({
   open,
   onOpenChange,
   onConfirm,
   showCloseButton = false,
-  errorMessage,
+  children,
+  className,
 }: ErrorModalProps) => {
   return (
     <Modal
       open={open}
       onOpenChange={onOpenChange}
       showCloseButton={showCloseButton}
-      className='max-w-[353px] px-4 pb-6 gap-10'
+      className={cn('max-w-[353px] px-4 pb-6 gap-10', className)}
     >
       <Modal.Header>
         <Modal.Title className='mt-2 flex justify-center custom-text-2lg-bold md:custom-text-xl-bold'>
-          {errorMessage}
+          {children}
         </Modal.Title>
       </Modal.Header>
       <Modal.Footer>
