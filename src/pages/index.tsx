c24b6fd0a33b1634cd3f61f0448ca63472ea752a
llmojoll <wnsgusqkqh21@gmail.com>
2025-07-28 17:08:13 +0900

    Feat : 테스트 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,11 @@ import Link from 'next/link';
 
 import { ContentSection } from '@/components/home/ContentSection';
 import { HeroSection } from '@/components/home/HeroSection';
-import EditWineModal from '@/components/Modal/WineModal/EditWineModal';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  const data = {
-    wineId: 1376,
-    name: 'Chateau Margaux',
-    price: 250000,
-    region: 'Bordeaux, France',
-    image: 'https://example.com/wine-image.jpg',
-    type: 'RED',
-    avgRating: 4.7,
-  } as const;
-
   return (
     <div className='mx-auto px-[16px] md:px-[20px] xl:px-0 max-w-[1140px]  min-w-[343px] mt-6 xl:mt-[80px]'>
-      <EditWineModal wine={data} />
       <HeroSection />
       <main>
         <ContentSection />
