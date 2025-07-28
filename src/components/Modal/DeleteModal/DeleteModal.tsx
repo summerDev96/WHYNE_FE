42632fb0c67b8d715ca36a973dfce641a470376a
llmojoll <wnsgusqkqh21@gmail.com>
2025-07-28 17:40:42 +0900

    Chore

---
+++ src/components/Modal/DeleteModal/DeleteModal.tsx
@@ -0,0 +1,9 @@
+import BasicModal from '@/components/common/Modal/BasicModal';
+
+export default function DeleteModal() {
+  return (
+    <div>
+      <BasicModal type='register' title='모달 제목' open={true} onOpenChange={} />
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link';
 
 import { ContentSection } from '@/components/home/ContentSection';
 import { HeroSection } from '@/components/home/HeroSection';
+import DeleteModal from '@/components/Modal/DeleteModal/DeleteModal';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div className='mx-auto px-[16px] md:px-[20px] xl:px-0 max-w-[1140px]  min-w-[343px] mt-6 xl:mt-[80px]'>
+      <DeleteModal />
       <HeroSection />
       <main>
         <ContentSection />
