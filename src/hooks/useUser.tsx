7285d8aece250a8e25d2c3e63a478e26606e09ca
youjin-hub <leeyoujin0704@naver.com>
2025-07-29 19:03:31 +0900

    feat: 전역 유저 상태에 접근할 수 있는 커스텀 hook 생성

---
+++ src/hooks/useUser.tsx
@@ -0,0 +1,12 @@
+import { useUserStore } from '@/stores/userStore';
+
+/**
+ * 전역 유저 상태를 쉽게 접근할 수 있는 커스텀 훅 */
+export const useUser = () => {
+  const user = useUserStore((state) => state.user);
+  const isLoggedIn = useUserStore((state) => state.isLoggedIn);
+  const setUser = useUserStore((state) => state.setUser);
+  const clearUser = useUserStore((state) => state.clearUser);
+
+  return { user, isLoggedIn, setUser, clearUser };
+};
