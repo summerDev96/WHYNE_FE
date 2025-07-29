11a44d8233c621911439aede5ae4a04258d0d62a
youjin-hub <leeyoujin0704@naver.com>
2025-07-30 04:32:39 +0900

    feat: profile image 기본값 설정, 호버 오버레이 구현

--- src/components/my-profile/ProfileImageInput.tsx
+++ src/components/my-profile/ProfileImageInput.tsx
@@ -1,11 +1,19 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+import Camera from '@/assets/camera.svg';
+import UserDefaultImg from '@/assets/icons/userDefaultImg.svg';
+
+interface ProfileImageInputProps {
+  /** 유저가 이미 프로필 이미지를 갖고 있다면 초기 이미지 URL */
+  defaultImageUrl?: string;
+}
 
 /**
- * 가장 기본적인 이미지 업로드 + 미리보기 컴포넌트
- * - 이미지를 클릭하면 파일 선택창이 열립니다.
- * - 파일을 선택하면 미리보기가 즉시 반영됩니다.
+ * 이미지 업로드 + 미리보기 컴포넌트
+ * - 이미지를 클릭하면 파일 선택창 오픈
+ * - 파일을 선택하면 미리보기가 즉시 반영
  */
-export function ProfileImageInput() {
+export function ProfileImageInput({ defaultImageUrl }: ProfileImageInputProps) {
   const fileInputRef = useRef<HTMLInputElement | null>(null); // 숨겨진 input 제어
   const [previewUrl, setPreviewUrl] = useState<string | null>(null); // 미리보기 이미지 URL
 
@@ -23,18 +31,29 @@ export function ProfileImageInput() {
     }
   };
 
+  /** 초기 이미지 설정 */
+  useEffect(() => {
+    if (defaultImageUrl) {
+      setPreviewUrl(defaultImageUrl);
+    }
+  }, [defaultImageUrl]);
+
   return (
     <div className='flex flex-col items-center gap-2'>
       {/* 이미지 미리보기 영역 */}
-      <div
-        onClick={handleImageClick}
-        className='w-32 h-32 rounded-full overflow-hidden border cursor-pointer'
-      >
-        <img
-          src={previewUrl ?? 'https://via.placeholder.com/150'}
-          alt='프로필 미리보기'
-          className='w-full h-full object-cover'
-        />
+      <div className='group relative overflow-hidden rounded-full w-16 h-16 xl:w-40 xl:h-40 cursor-pointer'>
+        <div onClick={handleImageClick} className='rounded-full border border-gray-300'>
+          {previewUrl ? (
+            <img src={previewUrl} alt='프로필 미리보기' className='w-full h-full object-cover' />
+          ) : (
+            <UserDefaultImg className='w-full h-full text-gray-100' />
+          )}
+        </div>
+
+        {/* Hover 시 보여줄 오버레이 */}
+        <div className='absolute inset-0 bg-primary/40 flex items-center justify-center opacity-0 group-hover:opacity-100 transition'>
+          <Camera className='w-4 h-4 text-white xl:w-8 xl:h-8' />
+        </div>
       </div>
 
       {/* 숨겨진 파일 업로드 input */}
