3ac62acfc5c405a558fa14ad7b37839bd936f1b4
llmojoll <wnsgusqkqh21@gmail.com>
2025-07-30 23:40:42 +0900

    Feat : 필터모달 바텀시트 적용

--- src/components/Modal/FilterModal/FilterModal.tsx
+++ src/components/Modal/FilterModal/FilterModal.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import BasicBottomSheet from '@/components/common/BottomSheet/BasicBottomSheet';
 import WineTypeFilter from '@/components/common/Filter/WineTypeFilter';
 import BasicModal from '@/components/common/Modal/BasicModal';
 import { Button } from '@/components/ui/button';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
 import useFilterStore from '@/stores/filterStore';
 
 const FilterModal = ({
@@ -13,47 +15,65 @@ const FilterModal = ({
   onOpenChange: (isOpen: boolean) => void;
 }) => {
   const reset = useFilterStore((state) => state.reset);
+  const isDesktop = useMediaQuery('(min-width: 640px)');
 
   const handleApplyFilter = (e: React.FormEvent) => {
     e.preventDefault();
     onOpenChange(false); // 필터 적용 후 모달을 close
   };
 
+  const renderButton = (
+    <div className='flex gap-2'>
+      <Button
+        onClick={reset}
+        variant='purpleLight'
+        size='xl'
+        className='w-[96px] md:w-[96px]'
+        fontSize='lg'
+      >
+        초기화
+      </Button>
+      <Button
+        type='button'
+        onClick={handleApplyFilter}
+        variant='purpleDark'
+        size='xl'
+        className='w-[223px] md:w-[223px]'
+        fontSize='lg'
+      >
+        필터 적용하기
+      </Button>
+    </div>
+  );
+
+  const renderForm = (
+    <form onSubmit={handleApplyFilter}>
+      <WineTypeFilter className='mt-[20px] mb-[25px] ' showBorder={true} hasMargin={false} />
+    </form>
+  );
+
   return (
     <div>
-      <BasicModal
-        type='filter'
-        title='필터'
-        open={open}
-        onOpenChange={onOpenChange}
-        buttons={
-          <div className='flex gap-2'>
-            <Button
-              onClick={reset}
-              variant='purpleLight'
-              size='xl'
-              className='w-[96px] md:w-[96px]'
-              fontSize='lg'
-            >
-              초기화
-            </Button>
-            <Button
-              type='button'
-              onClick={handleApplyFilter}
-              variant='purpleDark'
-              size='xl'
-              className='w-[223px] md:w-[223px]'
-              fontSize='lg'
-            >
-              필터 적용하기
-            </Button>
-          </div>
-        }
-      >
-        <form onSubmit={handleApplyFilter}>
-          <WineTypeFilter className='mt-[20px] mb-[25px] ' showBorder={true} hasMargin={false} />
-        </form>
-      </BasicModal>
+      {isDesktop ? (
+        <BasicModal
+          type='filter'
+          title='필터'
+          open={open}
+          onOpenChange={onOpenChange}
+          buttons={renderButton}
+        >
+          {renderForm}
+        </BasicModal>
+      ) : (
+        <BasicBottomSheet
+          title='필터'
+          open={open}
+          onOpenChange={onOpenChange}
+          buttons={renderButton}
+        >
+          {renderForm}
+        </BasicBottomSheet>
+      )}
     </div>
   );
 };
