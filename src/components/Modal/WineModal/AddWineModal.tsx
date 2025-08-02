696206a151317955fa1b263749d1a19217ccab23
이관현\Luganic <rhksgus509@gmail.com>
2025-08-02 16:53:37 +0900

    fix: 빌드

--- src/components/Modal/WineModal/AddWineModal.tsx
+++ src/components/Modal/WineModal/AddWineModal.tsx
@@ -13,7 +13,6 @@ import SelectDropdown from '../../common/dropdown/SelectDropdown';
 import Input from '../../common/Input';
 import BasicModal from '../../common/Modal/BasicModal';
 import { Button } from '../../ui/button';
-
 interface WineForm {
   wineName: string;
   winePrice: number;
@@ -21,23 +20,19 @@ interface WineForm {
   wineImage: FileList;
   wineType: string;
 }
-
 interface AddWineModalProps {
   showRegisterModal: boolean;
   setShowRegisterModal: (state: boolean) => void;
 }
-
 const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalProps) => {
   const [category, setCategory] = useState('');
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const [previewImage, setPreviewImage] = useState<string | null>(null);
   const queryClient = useQueryClient();
   const isDesktop = useMediaQuery('(min-width: 640px)');
-
   const triggerFileSelect = () => {
     fileInputRef.current?.click();
   };
-
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
@@ -47,7 +42,6 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
       setPreviewImage(null);
     }
   };
-
   const {
     register,
     handleSubmit,
@@ -59,7 +53,6 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
   } = useForm<WineForm>({
     mode: 'onBlur',
   });
-
   const resetForm = () => {
     reset({
       wineName: '',
@@ -72,10 +65,11 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
     setPreviewImage(null);
     if (fileInputRef.current) fileInputRef.current.value = '';
   };
-
   const handlePostWine = async (form: WineForm) => {
     const file = form.wineImage[0];
-    const imageUrl = await uploadImage(file);
+    const newFileName = file.name.replace(/\s/g, '-');
+    const newFile = new File([file], newFileName, { type: file.type });
+    const imageUrl = await uploadImage(newFile);
     const requestData: PostWineRequest = {
       name: form.wineName,
       region: form.wineOrigin,
@@ -85,7 +79,6 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
     };
     return postWine(requestData);
   };
-
   const postWineMutation = useMutation({
     mutationFn: handlePostWine,
     onSuccess: () => {
@@ -98,20 +91,15 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
       console.log('와인 등록 실패', error);
     },
   });
-
   const onSubmit = async (form: WineForm) => {
     postWineMutation.mutate(form);
   };
-
   const categoryOptions = [
     { label: 'Red', value: 'Red' },
     { label: 'White', value: 'White' },
     { label: 'Sparkling', value: 'Sparkling' },
   ];
-
   const selectedCategoryLabel = categoryOptions.find((opt) => opt.value === category)?.label;
-
-  //모달창 끄면 리셋되게
   const closeModalReset = (isOpen: boolean) => {
     setShowRegisterModal(isOpen);
     if (!isOpen) {
@@ -120,11 +108,8 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
       }, 50);
     }
   };
-  ////
-
   const renderForm = () => (
     <form onSubmit={handleSubmit(onSubmit)} encType='multipart/form-data' className='mx-2'>
-      {/* 이름 */}
       <p className='custom-text-md-medium md:custom-text-lg-medium mt-[22px] mb-[10px]'>
         와인 이름
       </p>
@@ -140,7 +125,6 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
         placeholder='와인 이름 입력'
         className='custom-text-md-regular md:custom-text-lg-regular'
       />
-      {/* 가격 */}
       <p className='custom-text-md-medium md:custom-text-lg-medium mt-[22px] mb-[10px]'>가격</p>
       <Input
         {...register('winePrice', {
@@ -154,7 +138,6 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
         placeholder='가격 입력'
         className='[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none custom-text-md-regular md:custom-text-lg-regular'
       />
-      {/* 원산지 */}
       <p className='custom-text-md-medium md:custom-text-lg-medium mt-[22px] mb-[10px]'>원산지</p>
       <Input
         {...register('wineOrigin', {
@@ -168,7 +151,6 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
         placeholder='원산지 입력'
         className='custom-text-md-regular md:custom-text-lg-regular'
       />
-      {/* 타입 */}
       <p className='custom-text-md-medium md:custom-text-lg-medium mt-[22px] mb-[10px]'>타입</p>
       <SelectDropdown
         selectedValue={category}
@@ -194,7 +176,7 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
         <div className='relative'>
           <p className='text-red-500 absolute mt-1'>{errors.wineType.message}</p>
         </div>
-      )}{' '}
+      )}
       <Input
         {...register('wineType', {
           required: '타입을 선택해 주세요.',
@@ -204,7 +186,6 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
         type='text'
         className='hidden'
       />
-      {/* 사진 */}
       <p className='custom-text-md-medium md:custom-text-lg-medium mt-[24px]'>와인 사진</p>
       <Input
         {...register('wineImage', {
@@ -229,7 +210,6 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
           onClick={triggerFileSelect}
         >
           {previewImage ? (
-            // eslint-disable-next-line @next/next/no-img-element
             <img src={previewImage} alt='미리보기' className='w-full h-full object-cover' />
           ) : (
             <div className='flex flex-col items-center text-gray-400'>
@@ -245,7 +225,6 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
       </div>
     </form>
   );
-
   const renderButton = (
     <div className='flex gap-2 w-full'>
       <Button
@@ -274,7 +253,6 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
       </Button>
     </div>
   );
-
   return isDesktop ? (
     <BasicModal
       type='register'
@@ -297,5 +275,4 @@ const AddWineModal = ({ showRegisterModal, setShowRegisterModal }: AddWineModalP
     </BasicBottomSheet>
   );
 };
-
 export default AddWineModal;
